2c1902274572abff386e6bf3da2cc37287418dfc
Rafael Santos <rafael_santos.s1@hotmail.com>
2023-08-09 22:57:24 -0300

    feat: ensure SignUpController returns 400 no name is provided

--- jest.config.ts
+++ jest.config.ts
@@ -2,11 +2,7 @@ import type { Config } from 'jest'
 
 const config: Config = {
 
-  clearMocks: true,
-  collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
