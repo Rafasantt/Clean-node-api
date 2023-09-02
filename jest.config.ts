bffe53a5434be529c647081476e9588cdd06ba22
Rafael Santos <rafael_santos.s1@hotmail.com>
2023-09-02 00:33:06 -0300

    feat: project suitability

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,13 @@ import type { Config } from 'jest'
 
 const config: Config = {
 
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/data/usecases/add-account/bd-add-account-protocols.ts',
+    '!<rootDir>/src/presentation/controllers/signup/signup-protocols.ts',
+    '!<rootDir>/src/presentation/protocols/index.ts'
+  ],
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
