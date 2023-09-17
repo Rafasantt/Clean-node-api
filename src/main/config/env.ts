8b22c6435f2e919412db633a58dcd93379727cca
Rafael Santos <rafael_santos.s1@hotmail.com>
2023-09-17 17:03:20 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || 'dfagrhsbsfvs234==3423-32344eds=323143=34edsaf3rfegbdc_e2212'
 }
