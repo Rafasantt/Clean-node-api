5672f3a3014fd280f05cfc151681a380e76619eb
Rafael Santos <rafael_santos.s1@hotmail.com>
2023-09-16 23:29:00 -0300

    feat: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -0,0 +1,15 @@
+import Jwt from 'jsonwebtoken'
+import type { Encrypter } from '../../../data/protocols/criptography/encrypter'
+
+export class JwtAdapter implements Encrypter {
+  private readonly secret: string
+
+  constructor (secret: string) {
+    this.secret = secret
+  }
+
+  async encrypt (value: string): Promise<string> {
+    Jwt.sign({ id: value }, this.secret)
+    return null
+  }
+}
