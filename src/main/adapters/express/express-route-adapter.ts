95933b69ec3e9b6de558e6890d06c1979286edbb
Rafael Santos <rafael_santos.s1@hotmail.com>
2023-09-23 23:43:26 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/main/adapters/express/express-route-adapter.ts
+++ src/main/adapters/express/express-route-adapter.ts
@@ -4,7 +4,8 @@ import type { Request, Response } from 'express'
 export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: httpRequest = {
-      body: req.body
+      body: req.body,
+      headers: req.headers
     }
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
