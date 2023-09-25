e2aa4c5683219b630f7bd75e8615abb09d517676
Rafael Santos <rafael_santos.s1@hotmail.com>
2023-09-24 21:35:52 -0300

    feat: ensure SignUp route returns 403 if no access token is provided

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -3,8 +3,11 @@ import type { Router } from 'express'
 import { adaptRoute } from '../adapters/express-route-adapter'
 import { makeSignUpController } from '../factories/controllers/login2/signup/signup-controller-factory'
 import { makeLoginController } from '../factories/controllers/login2/login/login-controller-factory'
+import { adaptMiddleware } from '../adapters/express-middleware-adapter'
+import { makeAuthMiddleware } from '../factories/middlewares/auth-middleware-factory'
 
 export default (router: Router): void => {
-  router.post('/signup', adaptRoute(makeSignUpController()))
+  const adminAuth = adaptMiddleware(makeAuthMiddleware('admin'))
+  router.post('/signup', adminAuth, adaptRoute(makeSignUpController()))
   router.post('/login', adaptRoute(makeLoginController()))
 }
